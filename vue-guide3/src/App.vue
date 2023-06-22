8d59e859baacdf852b4c3e607aaea240c43529b2
sato.taku <59036645+izumiyahuuma@users.noreply.github.com>
2023-06-23 07:55:04 +0900

    provide,injectまで

---
+++ vue-guide3/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { provide } from 'vue';
+import { todoKey, todos } from './useTodo';
+import TodoList from './components/TodoList.vue';
+
+// provide('todos', todos);
+provide(todoKey, todos);
+</script>
+
+<template>
+  <TodoList></TodoList>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue-guide3/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+import { todoKey } from '../useTodo';
+const state = inject(todoKey);
+if (!state) {
+  throw new Error('State is undefined');
+}
+const { todos, addTodo } = state;
+// const todos = inject('todos');
+// const _addTodo = inject<AddTodo>('addTodo');
+
+// const addTodo = (title: string) => {
+//   _addTodo(title);
+// };
+</script>
+
+<template>
+  <div>
+    <ul :style="{ listStyle: 'none' }">
+      <li
+        :style="{ backgroundColor: 'aliceblue', marginBottom: '10px' }"
+        v-for="todo in todos"
+      >
+        {{ todo.title }}
+      </li>
+    </ul>
+    <button @click="addTodo('add!')">Add</button>
+  </div>
+</template>
+
+<style scoped></style>
